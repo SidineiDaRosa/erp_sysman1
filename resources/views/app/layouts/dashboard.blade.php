b4c7d3bc9024d1ba7bc6570aa1b294e9302d5760
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-04 20:53:19 -0300

    dashboard

--- resources/views/app/layouts/dashboard.blade.php
+++ resources/views/app/layouts/dashboard.blade.php
@@ -4,56 +4,90 @@
 
     <div class="card">
         <div class="card-header">
-            <p class="mb-0">Empresa</p>
+
 
         </div>
         <div class="card-body">
             <div class="d-flex m-2 justify-content-around">
-                <span class="record">Empresa modelo</span>
+                <span class="record">sysman8-v1.0-2022</span>
             </div>
         </div>
-        <div class="card-footer d-flex justify-content-center">
-            <a href="???" class="btn btn-success btn-lg">
-                <i class="icofont-check mr-1"></i>
-                <h1>Sysman8 </h1>
-                <h2> sistema de cadastro de produtos e serviços</h2>
 
-            </a>
-        </div>
-        <div class="cards-dashboard" id="cardsboard">
-            <style>
-                #cardsboard {
-                    display:flexbox;
-                    flex-direction:row;
-                    height: 200px;
-                    background-color: red;
-                }
-            </style>
-            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Header</div>
-                <ul>
-                    <li>Cadastro de ordens de serviço</li>
-                    <li>Cadastro de empresas</li>
-                    <li>Cadastro de equipamentos</li>
-                </ul>
-            </div>
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Manutenção</div>
-                <ul>
-                    <li>acertividade nas manuteçôes</li>
-
-                    <i class="icofont-dart icofont-10x"></i>
-                </ul>
-            </div>
-            <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Manutenção</div>
-                <ul>
-                    <li>acertividade nas manuteçôes</li>
-                    </i><i class="icofont-automation icofont-10x"></i>
-                </ul>
-            </div>
-        </div>
     </div>
 
 
+    <div class="" id="cardsboard">
+        <style>
+            #cardsboard {
+                display: flex;
+                flex-direction: row;
+                height: auto;
+                background-color: rgb(235, 227, 227);
+                position: relative;
+                align-items: center;
+                text-align: center;
+
+            }
+
+            .divt {
+                height: 200px;
+                width: 30%;
+                margin: 1%;
+                background-color: rgb(119, 147, 172);
+                position: relative;
+            }
+
+
+
+            @media only screen and (max-width: 600px) {
+                body {
+                    background-color: lightblue;
+
+
+                }
+
+                #cardsboard {
+                    display: flex;
+                    flex-direction: column;
+                    height: auto;
+                    background-color: rgb(245, 235, 235);
+                    position: relative;
+
+                }
+
+                .divt {
+                    height: 200px;
+                    width: 100%;
+                    padding: 10px;
+                    background-color: rgb(149, 175, 197);
+                    position: relative;
+                }
+            }
+        </style>
+        <div class="divt">
+            </i><i class="icofont-automation icofont-5x"></i>
+            <p></p>
+            <label for="">Automação</label>
+            <p>
+                <label for="">Manutenção programada</label>
+
+        </div>
+        <div class="divt">
+
+
+            <i class="icofont-dart icofont-5x"></i>
+            <p></p>
+            <label for="">Acertividade</label>
+            <p>
+                <label for="">Acuraciadade</label>
+        </div>
+        <div class="divt">
+
+            <i class="icofont-help-robot icofont-5x"></i>
+            <p></p>
+            <label for="">Equipamentos</label>
+
+        </div>
+
+    </div>
 </main>
