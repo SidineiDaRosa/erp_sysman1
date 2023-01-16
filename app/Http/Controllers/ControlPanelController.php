78bc386236a1aea820eb32d2444d2e0bdfae0787
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-16 09:26:47 -0300

    12

--- app/Http/Controllers/ControlPanelController.php
+++ app/Http/Controllers/ControlPanelController.php
@@ -25,23 +25,28 @@ class ControlPanelController extends Controller
         $qnt = 1;
         //$produto = Produto::find($produtos); //busca o registro do produto com o id da entrada do produto
         $totRegPecEquip = PecasEquipamentos::select('id')->max('id');
-        $totRegPecEquip = 8;
+       // $totRegPecEquip = 13;
         $x = 1;
         while ($x <= $totRegPecEquip) {
             //echo "The number is: $x <br>";
 
             $numRegistroPecaEquip = PecasEquipamentos::find($x); //busca o registro do produto com o id da entrada do produto
+            if (!empty($numRegistroPecaEquip)) {//verifica se exite este registro
+                $numRegistroPecaEquip->horas_proxima_manutencao = $numRegistroPecaEquip->horas_proxima_manutencao + 10; // soma estoque antigo com a entrada de produto
+                $numRegistroPecaEquip->save();
+            } else {
+               // echo ('este registro é nullo');
+            }
 
-            $numRegistroPecaEquip->horas_proxima_manutencao = $numRegistroPecaEquip->horas_proxima_manutencao + 10; // soma estoque antigo com a entrada de produto
             //$produto->estoque_ideal = $produto->estoque_ideal - $request->input('quantidade'); // soma estoque antigo com a entrada de produto
             //dd($numRegistroPecaEquip );
-            $numRegistroPecaEquip->save();
+            // $numRegistroPecaEquip->save();
             //echo ($numRegistroPecaEquip) . "<br>";
             $x += 1;
         }
         if ($x = $totRegPecEquip) {
 
-             $x = 0;
+            $x = 0;
             return view('site.control_panel');
         }
         //$produto->estoque_ideal = $produto->estoque_ideal -($qnt); // soma estoque antigo com a entrada de produto
