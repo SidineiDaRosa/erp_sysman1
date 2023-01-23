f5cb53f060f6b0c75e87763af477e89289e9cf97
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-23 10:23:02 -0300

    12

--- resources/views/site/configuracoes.php
+++ resources/views/site/configuracoes.php
@@ -24,6 +24,14 @@
     <div class="sidebar-submenu-expanded" id="sidebar-submenu-expanded-dashboard">
         <a class="sidebar-submenu-expanded-a" href="{{ route('/') }}">voltar</a><br>
     </div>
+
+  <?php
+include "vendor/autoload.php";
+$generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+echo $generator->getBarcode(
+    '081231723897',
+    $generator::TYPE_CODE_128 );
+    ?>
 </body>
 
 </html>
