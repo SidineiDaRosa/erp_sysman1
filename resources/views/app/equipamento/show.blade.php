4f09d70b69ee419650aed7b66515ea9bc74b855c
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-23 22:18:50 -0300

    12

--- resources/views/app/equipamento/show.blade.php
+++ resources/views/app/equipamento/show.blade.php
@@ -9,48 +9,60 @@
                 <a class="btn btn-sm btn-primary" href="{{route('equipamento.index')}}" class="btn">
                     LISTAGEM
                 </a>
-    
+
                 <a class="btn btn-sm btn-primary" href="{{route('equipamento.create')}}" class="btn">
                     NOVA MARCA
                 </a>
             </div>
         </div>
-        
-        
+
+
         <div class="card-body">
-           
-                 
-                        ID:
-                        {{$equipamento->id}}<p></p>
-                    
-                        Nome:
-                        {{$equipamento->nome}}<p></p>
-                    
-                        DESCRIÇÂO:
-                        {{$equipamento->descricao}}<p></p>
-                   
-                        MARCA:
-                        {{$equipamento->marca->nome}}<p></p>
-                   
-                        Empresa:
-                        {{$equipamento->Empresa->razao_social}}<p></p>
 
-                        Data de fabricação:
-                        {{$equipamento->data_fabricacao}}<p></p>
-                        
-                  
-            
+
+            ID:
+            {{$equipamento->id}}
+            <p></p>
+
+            Nome:
+            {{$equipamento->nome}}
+            <p></p>
+
+            DESCRIÇÂO:
+            {{$equipamento->descricao}}
+            <p></p>
+
+            MARCA:
+            {{$equipamento->marca->nome}}
+            <p></p>
+
+            Empresa:
+            {{$equipamento->Empresa->razao_social}}
+            <p></p>
+
+            Data de fabricação:
+            {{$equipamento->data_fabricacao}}
+            <p></p>
+
+
+            {!! QrCode::size(100)->backgroundColor(255,90,0)->generate( $produto->id.'--'.$produto->nome) !!}</tr>
+            <hr>
+
+            <hr><?php
+
+                $protocolo = (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == "on") ? "https" : "http");
+                $url = '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+                $urlPaginaAtual = $protocolo . $url
+                //echo $protocolo.$url;
+                ?>
+            Visualisar no web site:
+            <p></p>
+            {!! QrCode::size(100)->backgroundColor(255,90,0)->generate( $urlPaginaAtual ) !!}
         </div>
 
-        </div>
+    </div>
     </div>
 
 </main>
 
 @endsection
-
-
-
-
-
-
