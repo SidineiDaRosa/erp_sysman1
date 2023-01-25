bb9f203fb163e2d7951c8f797950291857555e4f
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-25 19:21:39 -0300

    12

--- resources/views/app/estoque_produto/_components/form_create_edit.blade.php
+++ resources/views/app/estoque_produto/_components/form_create_edit.blade.php
@@ -56,7 +56,7 @@
         <div class="row mb-1">
             <label for="unidade_medida" class="col-md-4 col-form-label text-md-end text-right">Unid</label>
             <div class="col-md-6">
-                <input name="unidade_medida" id="unidade_medida" type="text" class="form-control " value="{{$produtos->unidade_medida_id}}">
+                <input name="unidade_medida" id="unidade_medida" type="text" class="form-control " value="{{$produtos->unidade_medida}}">
             </div>
         </div>
         <div class="row mb-1">
