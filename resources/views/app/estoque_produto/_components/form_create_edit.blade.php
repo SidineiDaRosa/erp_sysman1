a42f0f650f06cb580aaf0439e20bde5a2883b8bb
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-01-25 18:58:56 -0300

    12

--- resources/views/app/estoque_produto/_components/form_create_edit.blade.php
+++ resources/views/app/estoque_produto/_components/form_create_edit.blade.php
@@ -54,10 +54,11 @@
         @foreach
         ($unidades as $unidade)
         @endforeach
+        {{$unidade->nome}}
         <div class="row mb-1">
             <label for="unidade_medida" class="col-md-4 col-form-label text-md-end text-right">Unid</label>
             <div class="col-md-6">
-                <input name="unidade_medida" id="unidade_medida" type="text" class="form-control " value="$unidade->nome">
+                <input name="unidade_medida" id="unidade_medida" type="text" class="form-control " value="">
                
             </div>
         </div>
