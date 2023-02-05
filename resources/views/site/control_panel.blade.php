24ebf3bb5c20d38635878274cbab31026f64d538
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-02-05 14:42:43 -0300

    12

--- resources/views/site/control_panel.blade.php
+++ resources/views/site/control_panel.blade.php
@@ -1,17 +1,7 @@
 @extends('app.layouts.app')
 
 @section('content')
-<!DOCTYPE html>
 <html lang="pt">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel de controle
-
-    </title>
-</head>
 
 <body onload="checkCookies()">
     <h1>Painel de controle</h1>
