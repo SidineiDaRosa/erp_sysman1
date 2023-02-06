40ed2fb80c954a7aa9cc5388ebfc67ce4f264a64
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-02-06 20:22:10 -0300

    12

--- app/Http/Controllers/ControlPanelController.php
+++ app/Http/Controllers/ControlPanelController.php
@@ -19,8 +19,12 @@ use Illuminate\Http\Request;
 
 class ControlPanelController extends Controller
 {
+    
     public function index(Request $request)
+
     {
+        $equipamentos=Equipamento::all();
+        $produtos=Produto::all();
 
         $tipo_atualizacao = 0;
         // $dataAtual=Date('now',$timezone);
@@ -58,11 +62,11 @@ class ControlPanelController extends Controller
             //dd($ordens_servicos);
             $x = 0;
             $totRegPecEquip = 0;
-            return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos]);
+            return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos,'equipamentos'=>$equipamentos,'produtos'=>$produtos]);
         } else {
             $ordens_servicos = PecasEquipamentos::where('horas_proxima_manutencao', ('>='), 1)
                 ->where('horas_proxima_manutencao', ('<='), 4000)->get();
-            return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos]);
+            return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos,'equipamentos'=>$equipamentos,'produtos'=>$produtos]);
         }
         // }
         if ($tipo_atualizacao >= 1) {
