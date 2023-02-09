145edde506614bc6b1d74b098335b3f04c495708
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-02-09 15:31:56 -0300

    12

--- resources/views/app/pedido_saida/index.blade.php
+++ resources/views/app/pedido_saida/index.blade.php
@@ -5,6 +5,25 @@
     <div class="card">
         <div class="card-header-template">
             <div> Pedidos de saida</div>
+
+            <form id="formSearchingProducts" action="{{'Produtos-filtro'}}" method="POST">
+                @csrf
+                <div class="col-md-4 mb-0">
+                    <select class="form-control" name="tipofiltro" id="tipofiltro" value="" placeholder="Selecione o tipo de filtro">
+                        <option value="1">Busca Aberto</option>
+                        <option value="1">Busca Fechado</option>
+                        <option value="1">Busca Indefinido</option>
+                        <option value="1">Busca Em andamanto</option>
+                    </select>
+                </div>
+                <!--input box filtro buscar produto--------->
+
+                <input type="text" id="query" name="produto" placeholder="Buscar produto..." aria-label="Search through site content">
+                <button type="submit">
+                    <i class="icofont-search"></i>
+                </button>
+            </form>
+
             <div>
 
                 <a href="{{route('pedido-saida.create')}}" class="btn-sm btn-success">
@@ -18,14 +37,74 @@
 
         </div>
         <div class="card-body">
-            <table class="table-template table-striped table-hover table-bordered">
+            <style>
+                #formSearchingProducts {
+                    background-color: white;
+                    width: 900px;
+                    height: 44px;
+                    border-radius: 5px;
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                }
+
+                input {
+                    all: unset;
+                    font: 16px system-ui;
+                    color: blue;
+                    height: 100%;
+                    width: 100%;
+                    padding: 6px 10px;
+                }
+
+                ::placeholder {
+                    color: blueviolet;
+                    opacity: 0.9;
+                }
+
+
+                button {
+                    all: unset;
+                    cursor: pointer;
+                    width: 44px;
+                    height: 44px;
+                }
+
+                #tblPedidosSaida {
+                    font-family: arial, sans-serif;
+                    border-collapse: collapse;
+                    width: 100%;
+                    background-color: rgb(211, 211, 211);
+                    font-size: 20px;
+                }
+
+                thead {
+                    background-color: rgb(229, 228, 226);
+                }
+
+                td,
+                th {
+                    border: 1px solid #dddddd;
+                    text-align: left;
+                    padding: 8px;
+                    font-size: 15px;
+                }
+
+                tr:nth-child(even) {
+                    background-color: #dddddd;
+                }
+
+                tr:hover {
+                    background-color: rgb(169, 169, 169);
+                }
+            </style>
+            <table class="" id="tblPedidosSaida">
                 <thead>
                     <tr>
                         <th scope="col" class="th-title">Id</th>
                         <th scope="col" class="th-title">Data emissão</th>
-                        <th scope="col" class="th-title">hora emissão</th>
                         <th scope="col" class="th-title">Data prevista</th>
-                        <th scope="col" class="th-title">hora prevista</th>
+                        <th scope="col" class="th-title">Empresa</th>
                         <th scope="col" class="th-title">Equipamento</th>
                         <th scope="col" class="th-title">Emissor</th>
                         <th scope="col" class="th-title">status</th>
@@ -39,9 +118,8 @@
                     <tr>
                         <th scope="row">{{ $pedido_saida->id }}</td>
                         <td>{{ $pedido_saida->data_emissao }}</td>
-                        <td>{{ $pedido_saida->hora_emissao }}</td>
                         <td>{{ $pedido_saida->data_prevista}}</td>
-                        <td>{{ $pedido_saida->hora_prevista}}</td>
+                        <td>{{ $pedido_saida->empresa->nome_fantasia}}</td>
                         <td>{{ $pedido_saida->equipamento->nome}}</td>
                         <td>{{ $pedido_saida->funcionarios->primeiro_nome}}</td>
                         <td>{{ $pedido_saida->status}}</td>
