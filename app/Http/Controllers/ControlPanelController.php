d5a81534bdef77b18a819c52c16deab7cd57fa13
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-02-12 11:32:04 -0300

    12

--- app/Http/Controllers/ControlPanelController.php
+++ app/Http/Controllers/ControlPanelController.php
@@ -60,14 +60,14 @@ class ControlPanelController extends Controller
         }
         if ($x = $totRegPecEquip) {
             if (isset($horas_proxima_manutencao)) {
-                $ordens_servicos = PecasEquipamentos::where('horas_proxima_manutencao', ('>='), 1)
-                    ->where('horas_proxima_manutencao', ('<='), 4000)->get();
-                $x = 0;
-                $totRegPecEquip = 0;
-                return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos, 'equipamentos' => $equipamentos, 'produtos' => $produtos]);
-
+                echo('a varial exite');
                 if (!empty($horas_proxima_manutencao)) { //verifica se exite este registro
 
+                    $ordens_servicos = PecasEquipamentos::where('horas_proxima_manutencao', ('>='), 1)
+                        ->where('horas_proxima_manutencao', ('<='), 4000)->get();
+                    $x = 0;
+                    $totRegPecEquip = 0;
+                    return view('site.control_panel', ['ordens_servicos' =>  $ordens_servicos, 'equipamentos' => $equipamentos, 'produtos' => $produtos]);
                 }
             }
         } else {
