f7874ca3d0219d11acbc6436f6533fc13e9a67c0
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2023-02-19 10:50:52 -0300

    12

--- resources/views/app/peca_equipamento/index.blade.php
+++ resources/views/app/peca_equipamento/index.blade.php
@@ -136,7 +136,45 @@
         </table>
         <div class="card">
             Ordens de serviços abertos
-            {{$ordens_servicos}}
+            {{$ordens_servicos->id}}
+            <style>
+                table {
+                    font-family: arial, sans-serif;
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+
+                td,
+                th {
+                    border: 1px solid #dddddd;
+                    text-align: left;
+                    padding: 8px;
+                }
+
+                tr:nth-child(even) {
+                    background-color: #dddddd;
+                }
+            </style>
+            </head>
+
+            <body>
+
+                <h2>HTML Table</h2>
+
+                <table>
+                    <tr>
+                        <th>Company</th>
+                        <th>Contact</th>
+                        <th>Country</th>
+                    </tr>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                    </tr>
+
+
+                </table>
         </div>
 
 
